842813de521725b4929d3c0e5bde5758efab9d5d
Emily Braman <embraman10@gmail.com>
2020-06-14 20:52:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,7 +16,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   produce_storage= [CONVENTIONAL_PRODUCE,ORGANIC_PRODUCE]
-  produce_storage[1][3]
+  produce_storage[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
